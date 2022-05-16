e0eb08e7372e7a570f8c324a345fc7779b303f22
Michael Chaves <michaelchaves100@gmail.com>
2022-05-16 15:15:06 -0300

    e

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         Michael já fez aqui ---
         </p>
         <a
           className="App-link"
@@ -21,5 +21,6 @@ function App() {
     </div>
   );
 }
+
 
 export default App;
